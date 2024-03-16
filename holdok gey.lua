9a2796c01060b8bdc7e7f739d802b7df34551b11
Holdok11 <162834739+Holdok11@users.noreply.github.com>
2024-03-16 03:26:51 +0300

    Update and rename Hub.lua to holdok gey.lua

---
+++ holdok gey.lua
@@ -0,0 +1,26 @@
+local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
+ 
+local PhantomForcesWindow = Library:NewWindow("Holdok gey")
+ 
+local KillingCheats = PhantomForcesWindow:NewSection("Player")
+ 
+KillingCheats:CreateButton("Fly", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()
+end)
+
+KillingCheats:CreateButton("Inf eld", function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+end)
+
+KillingCheats:CreateButton("Dex", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua", true))()
+end)
+
+KillingCheats:CreateButton("Keyboard", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))()
+end)
+
+local KillingCheats = PhantomForcesWindow:NewSection("Esp")
+
+KillingCheats:CreateButton("Нету:(", function()
+end)
